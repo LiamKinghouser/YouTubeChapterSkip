8e9287dd796ee1b21c7b81d5d279dec410e8c98f
Juvoo <lkinghouser@gmail.com>
2023-04-18 18:08:20 -0500

    Commit

--- background.js
+++ background.js
@@ -8,21 +8,20 @@ function skipToNextChapter() {
     let searchStringIndex = videoURL.indexOf(searchString)
     let videoID = videoURL.slice(searchStringIndex + searchString.length, videoURL.indexOf('&t'))
 
-    let elements = document.getElementsByClassName("yt-simple-endpoint style-scope yt-formatted-string")
+    let elements = document.getElementsByClassName("yt-simple-endpoint style-scope ytd-macro-markers-list-item-renderer")
     let chapters = []
     let hrefs = []
     for (let i = 0; i < elements.length; i++) {
         let element = elements[i]
-        if (element.getAttribute("href") != null && element.getAttribute("href").toString().includes(videoID) && (element.innerText.includes(':') && /^[0-9]*$/.test(element.innerText.replaceAll(':', ''))) && !hrefs.includes(element.getAttribute("href"))) {
+        if (!element.hasAttribute('hidden href') && element.hasAttribute("href") && element.getAttribute("href").toString().includes(videoID) && element.getAttribute('href').toString().includes('&t=') && !hrefs.includes(element.getAttribute("href"))) {
             chapters.push(element)
             hrefs.push(element.getAttribute("href"))
         }
     }
+    if (chapters.length === 0) return
     let currentTimeSeconds = parseInt(document.getElementById('movie_player').getCurrentTime().toString().split('.')[0].replaceAll('.', ''))
 
     let nextChapterSeconds
-
-    console.log(currentTimeSeconds)
 
     for (let i = 0; i < chapters.length; i++) {
         let split = chapters[i].getAttribute('href').split('&t=')
@@ -38,17 +37,15 @@ function skipToNextChapter() {
 
 function triggered() {
     chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-        let youtubeTabs = []
         for (let i = 0; i < tabs.length; i++) {
-            if (tabs[i].url.includes("youtube.com/watch")) youtubeTabs.push(tabs[i])
-        }
-        if (youtubeTabs.length === 1) {
-            chrome.scripting.executeScript(
-                {
-                    target: {tabId: youtubeTabs[0].id},
-                    func: skipToNextChapter,
-                    world: "MAIN"
-                },() => {})
+            if (tabs[i].url.includes("www.youtube.com/watch")) {
+                chrome.scripting.executeScript(
+                    {
+                        target: {tabId: tabs[i].id},
+                        func: skipToNextChapter,
+                        world: "MAIN"
+                    },() => {})
+            }
         }
     })
 }
